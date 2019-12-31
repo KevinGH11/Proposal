d3eb7e48e709c6f3d88deee37351e8d671cc12e3
leb1996 <55517841+leb1996@users.noreply.github.com>
2020-01-01 03:19:28 +0800

    Update models.py
    Adding necessary fields into database

--- smartGarden/garden/models.py
+++ smartGarden/garden/models.py
@@ -2,7 +2,17 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-	
+        userName = models.CharField(max_length=20)
+        password = models.CharField(max_length=20)
+
 class Land(models.Model):
-	
+        LandID = models.PositiveSmallIntegerField(primary_key=True)
+        CurrentSoilMoisturelvl = models.PositiveSmallIntegerField()
+        CurrentLightIntensitylvl = models.PositiveSmallIntegerField()
+
 class Plant(models.Model):
+        LandID = models.ForeignKey(Land, on_delete=models.CASCADE)
+        PlantID = models.PositiveSmallIntegerField(primary_key=True)
+        plantName = models.CharField(max_length=20)
+        OptimalSoilMoisture = models.PositiveSmallIntegerField()
+        OptimalLightIntensity = models.PositiveSmallIntegerField()
