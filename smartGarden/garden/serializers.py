98bbe715b83319352e70f09d532c73a914778f1d
leb1996 <55517841+leb1996@users.noreply.github.com>
2020-01-01 03:21:36 +0800

    Update serializers.py
    Adding necessary fields to match models in models.py

--- smartGarden/garden/serializers.py
+++ smartGarden/garden/serializers.py
@@ -1,15 +1,17 @@
-6from rest_framework import serializers
+from rest_framework import serializers
 from . import models
 
 class UserSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = models.User
-		
+        class Meta:
+                model = models.User
+                fields = ('id', 'userName', 'password')
+
 class LandSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = models.Land
-		
+        class Meta:
+                model = models.Land
+                fields = ('LandID', 'CurrentSoilMoisturelvl', 'CurrentLightIntensity')
+
 class PlantSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = models.Plant
-		
+        class Meta:
+                model = models.Plant
+                fields = ('LandID', 'PlantID', 'plantName', 'OptimalSoilMoisture')
